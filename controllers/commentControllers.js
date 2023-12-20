27717d66fd7725a1792d2c1382dd0fcb8021dd3c
VJacobo <victoriajacobo99@gmail.com>
2023-12-20 16:21:03 -0600

    added main.handlebars

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
 // Update existing comment
 router.put('/:id', async (req, res) => {
     try {
-        const ( id ) = req.params;
+        const { id } = req.params;
         const { text } = req.params;
 
         const comment = await Comment.findByPk(id);
@@ -41,21 +41,22 @@ router.put('/:id', async (req, res) => {
 });
 
 // Delete a comment
-router.delete('/:id', async (req.res) => {
+router.delete('/:id', async (req, res) => {
     try {
-        const { id }  = req.params;
+        const { id } = req.params;
 
         const comment = await Comment.findByPk(id);
 
         if (!comment) {
-            return res.status(404).json({ message: ' Comment not found'});
+            return res.status(404).json({ message: 'Comment not found' });
         }
+        
         await comment.destroy();
 
-        res.json({ message: ' Comment deleted successfully'});
+        res.json({ message: 'Comment deleted successfully' });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: ' Internal Server Error'});
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 });
 
